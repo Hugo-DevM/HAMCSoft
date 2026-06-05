34f19e23a78f2b98a7ecd0f9cadaf95fbb63e2f6
Hugo Montaño <mobile.developer.mx@gmail.com>
2026-06-04 19:04:18 -0600

    Add redirects in next.config.js for domain canonicalization

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.hamcsoft.com" }],
+        destination: "https://hamcsoft.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
